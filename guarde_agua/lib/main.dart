87fb76022dc08f2860bf73855045e1faebd77847
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-07-27 18:07:53 -0300

    :lipstick: feat: criar a tela de recuperar senha etapa 3

--- guarde_agua/lib/main.dart
+++ guarde_agua/lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:guarde_agua/telaCadastro.dart';
 import 'package:guarde_agua/telaCadastro2.dart';
 import 'package:guarde_agua/telaCadastro3.dart';
 import 'package:guarde_agua/telaLogin.dart';
+import 'package:guarde_agua/view/tela_trocar_senha.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
       initialRoute: '/telalogin',
+      theme: ThemeData(fontFamily: GoogleFonts.lexend().fontFamily),
       routes: {
-        '/telalogin': (context) => TelaLogin(),
-        '/telaCadastro': (context) => TelaCadastro(),
-        '/telaCadastro2': (context) => TelaCadastro2(),
-        '/telaCadastro3': (context) => TelaCadastro3(),
+        '/telalogin': (context) => const TelaLogin(),
+        '/telaCadastro': (context) => const TelaCadastro(),
+        '/telaCadastro2': (context) => const TelaCadastro2(),
+        '/telaCadastro3': (context) => const TelaCadastro3(),
+        '/telaTrocarSenha': (context) => TelaTrocarSenha(),
       },
     ),
   );
